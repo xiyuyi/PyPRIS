81f3f309ef07179dd6a707e9b91df1afb01864b2
xiyu-pc-home <chinahelenyxy@gmail.com>
2018-11-06 09:10:06 -0800

    some more modifications

--- PyPRIS/solvers.py
+++ PyPRIS/solvers.py
@@ -8,48 +8,27 @@ class PRIS:
     """
     def __init__(self):
         self.type = 'PRIS solver'
-        self.refinement_number = 5  # default total number of refinement
-        self.refinement_factor_list = [1, 2, 2, 2, 2]  # default list of refinement factors
-        self.sparse_solver = 'lbreg'  # default choice of lasso solver. Only available one currently.
-        self.dual_channel = 'Yes'  # default setting of the number of observation channels.
         self.A = []  # sensing matrix
         self.b = []  # observation
         self.x = torch.empty(1, 1, dtype=torch.float)
         self.sparse_recovery_opts = []
         self.current_pris_iteration = 0
 
-    def set_pris_options(self, pris_options):
-        """
-        setup the pris options with input options. Otherwise, it will be the default options
+    def pris_loader(self, inputs):
 
-        :param pris_options: the input pris_options.
-        :return:
-
-        """
-        self.refinement_number = pris_options.refinement_number  # total number of refinement
-        self.refinement_factor_list = pris_options.refinement_factor_list  # list of refinement factors
-        self.lasso_solver = pris_options.lasso_solver  # choice of lasso solver. Only available one currently.
-        self.dual_channel = pris_options.dual_channel
-
-    def set_sparse_recovery_options(self, A, b, sparse_recovery_opts):
-        self.A = A
-        self.b = b
-        self.x = np.ndarray()
-        self.sparse_recovery_opts = sparse_recovery_opts
-
-    def pris_refine(self, pris_options, psf_model, coordinates_refiner):
+    def pris_refine(self, pris_options, psf_model, selector, refiner):
         # based on the iteration number, do the following:
         # identify the selected voxels
-        select(self, pris_options)
+        selector(self, pris_options)
         # refine the selected candidate voxels.
         refiner(self, pris_options)
         # update A using all the candidate voxels, and PSF model.
         self.A = psf_model(self)
 
-    def pris_go(self, sparse_recovery_solver):
+    def pris_go(self, subsolver):
         # based on the refined sensing matrix, perform sparse-recovery
         self.current_pris_iteration = self.current_pris_iteration + 1  # Pris iteration number tag increase by one.
-        self.x = sparse_recovery_solver(self.A, self.b)
+        self.x = subsolver(self.A, self.b)
 
 
 class PrisOpts:
@@ -61,6 +40,13 @@ class PrisOpts:
         self.type = 'Parameters'
         self.refinement_number = 5  # default total number of refinement
         self.refinement_factor_list = [1, 2, 2, 2, 2]  # default list of refinement factors
-        self.lasso_solver = 'lbreg'  # default choice of lasso solver. Only available one currently.
+        self.subsolver = 'lbreg'  # default choice of lasso solver. Only available one currently.
         self.dual_channel = 'Yes'  # default setting of the number of observation channels.
+
+class Selector:
+    """
+
+    """
+    def __init__(self, pris_options, ):
+        self.type = 'PRIS selector'
 
