b126312a255f0265b365acfce6d1a08a9cf6d642
DESKTOP-LP6C6RV\xiyu <chinahelenyxy@gmail.com>
2019-04-30 11:24:56 -0700

    modified LoadCSSolver

---
+++ pris_vis.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("/u/home/x/xiyuyi/bin")
+from PyPRIS import *
+
+path = ""
+PyPRIS_name = ""
+PyPRIS_SensMx_name = ""
+
+pris = loadCSSolver(path, PyPRIS_name, PyPRIS_SensMx_name)
