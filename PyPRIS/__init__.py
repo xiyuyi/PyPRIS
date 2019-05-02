6c16e1dda30fba14fd78361bb1155ab316107be8
DESKTOP-LP6C6RV\xiyu <chinahelenyxy@gmail.com>
2019-05-02 11:44:27 -0700

    fixed typo

--- PyPRIS/__init__.py
+++ PyPRIS/__init__.py
@@ -1,3 +1,8 @@
 from PyPRIS.PRIS import *
 from PyPRIS.module_observe_station import *
 from PyPRIS.config_tickets import *
+import numpy as np
+from skimage import io
+import copy
+import time
+import matplotlib
