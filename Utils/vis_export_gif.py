af18047910174c0698e75dcba279630e7c7a87f3
DESKTOP-LP6C6RV\xiyu <chinahelenyxy@gmail.com>
2019-05-03 17:26:37 -0700

    visualization pipeline for sets of tickets

--- Utils/vis_export_gif.py
+++ Utils/vis_export_gif.py
@@ -3,39 +3,69 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+thedir = 'G:\\DH_localization\\PyPRIS_tickets_F2_set1'
 
-fitem="bgSCF1.5_mu7.5e+09_alpha5.0e-09"
-path_d="G:/DH_localization/PyPRIS_tickets_set4/"+fitem+"/saved_objects"
-#r = imageio.get_writer(path_d + '_3projections.gif', mode='I')
-r = imageio.get_writer(path_d + '_plots.gif', mode='I')
+f = [name for name in os.listdir(thedir) if os.path.isdir(os.path.join(thedir, name))]
+# f = list()
+# f.append("bgSCF0.5_mu1.0e+10_alpha1.0e-09")
+# f.append("bgSCF0.5_mu1.0e+10_alpha1.0e-10")
+# f.append("bgSCF0.5_mu1.0e+10_alpha1.0e-11")
+# f.append("bgSCF0.8_mu1.0e+10_alpha1.0e-09")
+# f.append("bgSCF0.8_mu1.0e+10_alpha1.0e-10")
+# f.append("bgSCF0.8_mu1.0e+10_alpha1.0e-11")
+# f.append("bgSCF1.2_mu1.0e+10_alpha1.0e-09")
+# f.append("bgSCF1.2_mu1.0e+10_alpha1.0e-10")
+# f.append("bgSCF1.2_mu1.0e+10_alpha1.0e-11")
+# f.append("bgSCF1_mu1.0e+10_alpha1.0e-09")
+# f.append("bgSCF1_mu1.0e+10_alpha1.0e-10")
+# f.append("bgSCF1_mu1.0e+10_alpha1.0e-11")
 
+for fitem in f:
+    path_d= thedir + "/"+fitem+"/saved_objects"
+    for prisIter in np.arange(0,6):
+        r1 = imageio.get_writer(path_d + '_3projections_pris'+str(prisIter)+'.gif', mode='I')
+        r2 = imageio.get_writer(path_d + '_plots_pris'+str(prisIter)+'.gif', mode='I')
+        fnames1 = list()
+        fnames2 = list()
+        for itN in np.arange(1,908002,2000):
+            fnames1.append("PyPRIS__Proj_vies_"+fitem+"_pris"+str(prisIter)+"_plots_it"+str(itN)+".png")
+            fnames2.append("PyPRIS_"+fitem+"_pris"+str(prisIter)+"_plots_it"+str(itN)+"visualize.png")
 
-fnames=list()
-for itN in np.arange(1,908002,2000):
-    #fnames.append("PyPRIS__Proj_vies_"+fitem+"_pris0_plots_it"+str(itN)+".png")
-    fnames.append("PyPRIS_"+fitem+"_pris0_plots_it"+str(itN)+"visualize.png")
+        for fname in fnames1:
+            try:
+                im = imageio.imread(path_d+"/"+fname)
+                f = plt.figure()
+                ax = f.add_subplot(111)
+                plt.imshow(im)
+                plt.text(-0.3, 1.1,fname, ha='left', va='top', transform=ax.transAxes)
+                # Make a random plot...
+                # If we haven't already shown or saved the plot, then we need to
+                # draw the figure first...
+                f.canvas.draw()
+                # Now we can save it to a numpy array.
+                data = np.frombuffer(f.canvas.tostring_rgb(), dtype=np.uint8)
+                data = data.reshape(f.canvas.get_width_height()[::-1] + (3,))
+                plt.close()
+                r1.append_data(data)
+            except:
+                pass
 
-for fname in fnames:
-    try:
-        im = imageio.imread(path_d+"/"+fname)
-        f = plt.figure()
-        ax = f.add_subplot(111)
-        plt.imshow(im)
-        plt.text(-0.3, 1.1,fname, ha='left', va='top', transform=ax.transAxes)
-        # Make a random plot...
-        # If we haven't already shown or saved the plot, then we need to
-        # draw the figure first...
-        f.canvas.draw()
-
-        # Now we can save it to a numpy array.
-        data = np.frombuffer(f.canvas.tostring_rgb(), dtype=np.uint8)
-        data = data.reshape(f.canvas.get_width_height()[::-1] + (3,))
-        plt.close()
-        r.append_data(data)
-    except:
-        pass
-# produce a series of images into
-# with imageio.get_writer(path_d+'_3projections_movie.gif', mode='I') as writer:
-#     for filename in filenames:
-#         image = imageio.imread(filename)
-#         writer.append_data(image)d_data(image)
+        for fname in fnames2:
+            try:
+                im = imageio.imread(path_d+"/"+fname)
+                f = plt.figure()
+                ax = f.add_subplot(111)
+                plt.imshow(im)
+                plt.text(-0.3, 1.1,fname, ha='left', va='top', transform=ax.transAxes)
+                # Make a random plot...
+                # If we haven't already shown or saved the plot, then we need to
+                # draw the figure first...
+                f.canvas.draw()
+                # Now we can save it to a numpy array.
+                data = np.frombuffer(f.canvas.tostring_rgb(), dtype=np.uint8)
+                data = data.reshape(f.canvas.get_width_height()[::-1] + (3,))
+                plt.close()
+                r2.append_data(data)
+            except:
+                pass
