a7931a0d6ae19a9bb874b5ee978a33f04ef1fdf9
Xiyu Yi <xiyuyi@Xiyus-MacBook-Pro.local>
2019-05-18 13:24:45 -0700

    ready to stitch

---
+++ Utils/stitch_F3_fov1234.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("G:/DH_localization/PyPRIS") # for local tests.
+sys.path.append("~/Desktop/PyPRIS")
+from PyPRIS import *
+import os
+print("")
+print("")
+print("")
+# stitch 4 field of views together.
+p_fov1 = ""
