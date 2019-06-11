0cc0a29b6d51886fca9bff5bcbdc0ae884bac210
DESKTOP-LP6C6RV\xiyu <chinahelenyxy@gmail.com>
2019-06-11 11:21:20 -0700

    use trimmed PSF without subtracting the background

---
+++ test_dataset_4/trimPSF_v2.m
@@ -0,0 +1,32 @@
+% trim  PSF
+f1 = 'psf_BP+250.tif';
+f2 = 'psf_BP-250.tif';
+c1 = 91;
+c2 = 41;
+
+f = f1;
+c = c1;
+for i0 = 1:c-41
+    imwrite(ones(61,61,'uint16').*100,['trimmed_',f],'writemode','append');
+end
+for i0 = c-40:c+40
+    im = imread(f, 'Index', i0);
+    imwrite(im,['trimmed2_',f],'writemode','append');
+end
+for i0 = c+41:151
+    imwrite(ones(61,61,'uint16').*100,['trimmed_',f],'writemode','append');
+end
+
+
+f = f2;
+c = c2;
+for i0 = 1:c-41
+    imwrite(zeros(61,61,'uint16'),['trimmed_',f],'writemode','append');
+end
+for i0 = c-40:c+40
+    im = imread(f, 'Index', i0);
+    imwrite(im,['trimmed2_',f],'writemode','append');
+end
+for i0 = c+41:151
+    imwrite(ones(61,61,'uint16').*100,['trimmed_',f],'writemode','append');
+end
