18790f32a8325a0b2cf01bb5a88a275e2ee97152
Xiyu Yi <xiyu.yi@gmail.com>
2020-04-08 21:06:22 -0700

    Update __init__.py

--- PyPRIS/__init__.py
+++ PyPRIS/__init__.py
@@ -1,9 +1,10 @@
-from PyPRIS.PRIS import *
-from PyPRIS.observe_station import *
-from PyPRIS.config_tickets import *
-from PyPRIS.candidate_screening import *
 import numpy as np
 from skimage import io
 import copy
 import time
 import matplotlib
+matplotlib.use('agg')
+from PyPRIS.PRIS import *
+from PyPRIS.observe_station import *
+from PyPRIS.config_tickets import *
+from PyPRIS.candidate_screening import *
