ac140a41b1dd8c61d424ecd76cdead7c3a33f13a
Yi, Xiyu <yi10@llnl.gov>
2020-10-20 14:53:34 -0700

    .

--- PyPRIS/utils.py
+++ PyPRIS/utils.py
@@ -2,6 +2,10 @@ import os
 import pandas as pd
 import numpy as np
 import copy
+from csv import writer
+from sklearn.cluster import DBSCAN
+import pickle
+
 try:
     from matplotlib import pyplot as plt
     plt.switch_backend('agg')
@@ -109,3 +113,54 @@ def candidates_init_pop_and_roll(p):
     """
     print('OK')
     pass
+
+
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
+
+def get_clusters(locs, brightnesses, epsv, ticketIndex, csvpath, saveoption):
+    db = DBSCAN(eps=epsv, min_samples=1).fit(locs)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    clusters = []
+    # now check the kth cluster
+    for k in set(labels):
+        class_member_mask = (labels == k)
+        xy = locs[class_member_mask & core_samples_mask]
+        br = brightnesses[class_member_mask & core_samples_mask]
+        xyc = np.mean(xy, axis=0)
+        brc = np.sum(br)
+        clusters.append([xyc[0], xyc[1], brc])
+        therow = [ticketIndex, xyc[0], xyc[1], brc]
+        if saveoption is True:
+            append_list_as_row(file_name=csvpath, list_of_elem=therow)
+    return clusters
+
+
+def get_clusters_fromlinbreg(lbpath, ticketIndex, csvpath, D, save=False):
+    """
+    get clusters from the full path of a single linbreg object.
+    :param lbpath: full path to the targeted linbreg object
+    :param ticketIndex: this will be the index of the ticket that goes into the csv vile.
+    :param csvpath: full path of the csv file to append the fitting results
+    :param D: dimension [int]
+    :return: fitted clusters as a list
+    """
+    if D == 2:
+        # perform clustering using 2D only
+        with open(lbpath, 'rb') as f:
+            lb = pickle.load(f)
+        locs=np.asarray(lb.candidate_coords)[:,1:3]
+        brightnesses=lb.x[0:-1]
+        epsv=np.linalg.norm(lb.candidate_intervals[1:])
+        clusters = get_clusters(locs,brightnesses, epsv=epsv, ticketIndex=ticketIndex, csvpath=csvpath, saveoption=save)
+    else:
+        print('Sorry, only 2D clustering is available as of yet')
+    return clusters
---
+++ PyPRIS/vis.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+import numpy as np
+import copy
+from csv import writer
+from sklearn.cluster import DBSCAN
+import pickle
+from bokeh.plotting import figure, output_file, show, output_notebook
+import bokeh.io
+from bokeh.layouts import gridplot
+
+
+def pris_show(locs, lb, outputhtml, saveoption):
+    locs=np.asarray(locs)
+    blur = lb.b.reshape(128, 128)
+    recb = lb.recb.reshape(128, 128)
+    w=128
+    h=128
+    pw=450
+    ph=450
+    p1 = figure(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],
+                title='blur')
+    p1.x_range.range_padding = p1.y_range.range_padding = 0
+    p1.image(image=[blur], x=0, y=0, dw=w, dh=h)
+    # p1.scatter(locs[:,1],locs[:,0],marker='cross', color="red", size=1)
+
+    p2 = figure(x_range=p1.x_range, y_range=p1.y_range,
+                tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],
+                title='reconstructed blur')
+    p2.x_range.range_padding = p2.y_range.range_padding = 0
+    p2.image(image=[recb], x=0, y=0, dw=w, dh=h)
+
+    p3 = figure(x_range=p1.x_range, y_range=p1.y_range,
+                tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],
+                title='blur + fits')
+    p3.x_range.range_padding = p3.y_range.range_padding = 0
+    p3.image(image=[blur], x=0, y=0, dw=w, dh=h)
+    p3.scatter(locs[:, 1], locs[:, 0], marker='dot', size=25, color='red')
+
+    p4 = figure(x_range=p1.x_range, y_range=p1.y_range,
+                tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],
+                title='residual')
+    p4.x_range.range_padding = p4.y_range.range_padding = 0
+    p4.image(image=[blur - recb], x=0, y=0, dw=w, dh=h)
+    if saveoption:
+        output_file(outputhtml, title="pris result")
+    grid = gridplot([[p1, p2], [p4, p3]], plot_width=pw, plot_height=ph)
+    show(grid)
+    return
